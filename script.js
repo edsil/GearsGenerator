14b4eb54307eeb75b3ef23bd8f63ca3cad8af830
Eduardo <silva.eduardo@gmail.com>
2025-07-08 19:28:02 +0100

    user input working

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 let canvas, ctx;
 let divInfo1, divInfo2, divInfo3, divInfo4;
+let eldrawbtn;
 let elbgcolor, elfbcolor;
-let eldents, elproport;
+let eldents, elextdentarc, eldentlen, elintfactor, elextfactor;
 let elradius, elcircles, elcircradius;
+let elrotspeed;
+let dents, extdentarc, dentlen, intfactor, extfactor;
+let radius, circles, circradius, rotspeed;
+let r0, r1, radi;
 let h, w;
 let mouse = { x: 0, y: 0 };
 let initTime = Date.now();
 let lastUpdateTime = initTime;
 let frames = 0;
 let fps;
+let hitDraw = false;
 const bgImg = new Image();
 bgImg.src = "./blueprint-clean-background.png";
 const backgroundColor = "#0000";
 const drawing = "#FFFFFF";
 
-const lineWidth = 1;
+const lineWidth = 2;
 var choosenGear = 6;
 var gears = new Array(7);
 var sp = 0.0;
@@ -33,6 +39,7 @@ window.onload = function () {
 function initDomElements() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
+  eldrawbtn = document.getElementById("draw");
   divInfo1 = document.getElementById("info1");
   divInfo1.innerHTML = "Mouse";
   divInfo2 = document.getElementById("info2");
@@ -42,13 +49,71 @@ function initDomElements() {
   elbgcolor = document.getElementById("bgcolor");
   elfbcolor = document.getElementById("fbcolor");
   eldents = document.getElementById("dents");
-  elproport = document.getElementById("proport");
-  elradius = new Array(9);
-  for (var i = 0; i < 9; i++) {
+
+  elextdentarc = document.getElementById("extdentarc");
+  eldentlen = document.getElementById("dentlen");
+  elintfactor = document.getElementById("intfactor");
+  elextfactor = document.getElementById("extfactor");
+  elradius = new Array(7);
+  for (var i = 0; i < 7; i++) {
     elradius[i] = document.getElementById("r" + i);
   }
   elcircles = document.getElementById("circles");
   elcircradius = document.getElementById("circlesRadius");
+  elrotspeed = document.getElementById("rotspeed");
+}
+
+function readUser() {
+  dents = eldents.value * 1;
+  extdentarc = elextdentarc.value * 1;
+  dentlen = eldentlen.value * 1;
+  intfactor = elintfactor.value * 1;
+  extfactor = elextfactor.value * 1;
+  r0 = (extdentarc * dents) / Math.PI;
+  r1 = ((extdentarc - dentlen) * dents) / Math.PI;
+  radi = new Array(7);
+  for (var i = 0; i < 7; i++) {
+    radi[i] = elradius[i].value * 1;
+  }
+  circles = elcircles.value * 1;
+  circradius = elcircradius.value * 1;
+  choosenGear = 7;
+  gears[7] = [
+    w / 2,
+    h / 2,
+    dents,
+    proport,
+    [r0, r1, ...radi],
+    circles,
+    circradius,
+    0,
+    0,
+  ];
+  w = h = r0 * 2 + lineWidth * 2;
+  sp = elrotspeed.value * 1;
+  resize();
+}
+
+function readUserAndDraw() {
+  hitDraw = true;
+  readUser();
+  drawAfterUserInput();
+}
+
+function drawAfterUserInput(ts) {
+  drawGear2(
+    w / 2,
+    h / 2,
+    dents,
+    extdentarc,
+    dentlen,
+    intfactor,
+    extfactor,
+    radi,
+    circles,
+    circradius,
+    (sp * ts) / 1000
+  );
 }
 
 function initListeners() {
@@ -58,10 +123,10 @@ function initListeners() {
   window.onmousedown = mouseDown;
   window.onmouseup = mouseUp;
   window.onmousemove = mouseMove;
+  eldrawbtn.onmousedown = readUserAndDraw;
 }
 
 function resize() {
-
   /*
   w = canvas.width = window.innerWidth - canvas.offsetLeft * 2;
   h = canvas.height = window.innerHeight - canvas.offsetTop - 47;
@@ -72,12 +137,12 @@ function resize() {
   }
   ctx.scale(1920/w, 1080/h);
   */
-  w = h = gears[choosenGear][4][0] * 2+lineWidth+1;
+  w = h = gears[choosenGear][4][0] * 2 + lineWidth * 2;
   canvas.width = w;
   canvas.height = h;
   //w=1920;
   //h=1080;
-  
+
   divInfo2.innerHTML = "Width: " + w + "<br>Height: " + h;
 }
 
@@ -133,32 +198,99 @@ function animate(ts) {
   requestAnimationFrame(animate);
 }
 
-function readUserAndDraw(){
-  w = h = elradius[0].value * 2 + lineWidth + 1;
-  let dents = eldents.value * 1;
-  
-  gears[7] = [w/2,h/2,dents,proport,[240,224,128,112,56,32,16,16],8,0.9,0,0];
-}
-
-function setGears(){
-gears[0] = [w/2,h/2,32,proport,[240,224,128,112,56,32,16,16],8,0.9,0,0];
-gears[1] = [w/2,h/2,28,proport,[210,194,112,98,64,32,32,32],7,0.9,0,0];
-gears[2] = [w/2,h/2,24,proport,[180,164,96,84,64,32,32,32],6,0.9,0,0];
-gears[3] = [w/2,h/2,20,proport,[150,134,80,70,16,16,16,16],5,0.9,0,0];
-gears[4] = [w/2,h/2,16,proport,[120,104,64,56,24,24,24,24],4,0.9,0,0];
-gears[5] = [w/2,h/2,12,proport,[90,74,48,42,32,8,8,8],3,0.9,0,0];
-gears[6] = [w/2,h/2,8,proport,[60,44,32,28,16,16,16,16],2,0.9,0,0];
+function setGears() {
+  gears[0] = [
+    w / 2,
+    h / 2,
+    32,
+    proport,
+    [240, 224, 128, 112, 56, 32, 16, 16],
+    8,
+    0.9,
+    0,
+    0,
+  ];
+  gears[1] = [
+    w / 2,
+    h / 2,
+    28,
+    proport,
+    [210, 194, 112, 98, 64, 32, 32, 32],
+    7,
+    0.9,
+    0,
+    0,
+  ];
+  gears[2] = [
+    w / 2,
+    h / 2,
+    24,
+    proport,
+    [180, 164, 96, 84, 64, 32, 32, 32],
+    6,
+    0.9,
+    0,
+    0,
+  ];
+  gears[3] = [
+    w / 2,
+    h / 2,
+    20,
+    proport,
+    [150, 134, 80, 70, 16, 16, 16, 16],
+    5,
+    0.9,
+    0,
+    0,
+  ];
+  gears[4] = [
+    w / 2,
+    h / 2,
+    16,
+    proport,
+    [120, 104, 64, 56, 24, 24, 24, 24],
+    4,
+    0.9,
+    0,
+    0,
+  ];
+  gears[5] = [
+    w / 2,
+    h / 2,
+    12,
+    proport,
+    [90, 74, 48, 42, 32, 8, 8, 8],
+    3,
+    0.9,
+    0,
+    0,
+  ];
+  gears[6] = [
+    w / 2,
+    h / 2,
+    8,
+    proport,
+    [60, 44, 32, 28, 16, 16, 16, 16],
+    2,
+    0.9,
+    0,
+    0,
+  ];
 }
 
 function draw(ts) {
   ctx.fillStyle = backgroundColor;
   ctx.fillRect(0, 0, w, h);
-  //ctx.drawImage(bgImg, 0, 0);
+  if (sp != 0) ctx.drawImage(bgImg, 0, 0);
   ctx.strokeStyle = drawing;
   ctx.lineWidth = lineWidth;
-  for (var i = choosenGear; i < choosenGear+1; i++) {
+  if (hitDraw) {
+    drawAfterUserInput(ts);
+    return;
+  }
+  for (var i = choosenGear; i < choosenGear + 1; i++) {
     var g = gears[i];
-    var angleOffset = ((g[7]*ts / 1000) % (Math.PI * 2));
+    var angleOffset = ((g[7] * ts) / 1000) % (Math.PI * 2);
     drawGear(g[0], g[1], g[2], g[3], g[4], g[5], g[6], angleOffset + g[8]);
   }
 }
@@ -202,6 +334,63 @@ function drawGear(
   }
   var maxRadius = (r[3] - r[4]) / 2;
   var centerRadius = r[4] + maxRadius;
+  var radius = maxRadius * circlesRadius;
+  for (let i = 0; i < numCircles; i++) {
+    let angle = (i * Math.PI * 2) / numCircles;
+    let circleX = x + Math.cos(angle) * centerRadius;
+    let circleY = y + Math.sin(angle) * centerRadius;
+    ctx.beginPath();
+    ctx.arc(circleX, circleY, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+  ctx.restore();
+  readUserAndDraw();
+}
+
+function drawGear2(
+  x,
+  y,
+  dents,
+  extdentarc,
+  dentlen,
+  intfactor,
+  extfactor,
+  r,
+  numCircles,
+  circlesRadius,
+  angleOffset = 0
+) {
+  var bend = 0.3;
+  ctx.save();
+  ctx.translate(x, y);
+  ctx.rotate(angleOffset);
+
+  x = 0;
+  y = 0;
+  let r0 = (extdentarc * dents) / Math.PI;
+  let r1 = r0 - dentlen;
+  ctx.beginPath();
+  let dentArcAngle = (2 * Math.PI) / dents;
+  for (let i = 0; i < dents; i++) {
+    let angleStart0 = dentArcAngle * (i + 0.5 * (1 - extfactor));
+    let angleEnd0 = dentArcAngle * (i + 0.5 - 0.5 * (1 - extfactor));
+    let angleStart1 = dentArcAngle * (i + 0.5 + 0.5 * (1 - intfactor));
+    let angleEnd1 = dentArcAngle * (i + 1 - 0.5 * (1 - intfactor));
+
+    ctx.arc(x, y, r0, angleStart0, angleEnd0, false);
+    ctx.arc(x, y, r1, angleStart1, angleEnd1, false);
+  }
+  let ang0 = dentArcAngle * (0.5 * (1 - extfactor));
+  ctx.lineTo(x + r0 * Math.cos(ang0), y + r0 * Math.sin(ang0));
+  ctx.stroke();
+
+  for (let i = 0; i < r.length; i++) {
+    ctx.beginPath();
+    ctx.arc(x, y, r[i] * r1, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+  var maxRadius = (r1 * (r[1] - r[2])) / 2;
+  var centerRadius = r1 * r[2] + maxRadius;
   var radius = maxRadius * circlesRadius;
   for (let i = 0; i < numCircles; i++) {
     let angle = (i * Math.PI * 2) / numCircles;
